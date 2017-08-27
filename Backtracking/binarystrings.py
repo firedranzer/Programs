dee0b24cfc439898d3c45e649affee7fc800b3f9
firedranzer <rupeshharode@gmail.com>
2017-08-27 20:36:39 +0530

    minor changes

--- Backtracking/binarystrings.py
+++ Backtracking/binarystrings.py
@@ -1,8 +1,10 @@
 #Programs to display all binary strings of n-bits
 
+#function to append digits
 def appendtofront(x,l):
     return [x + i for i in l]
 
+#Function to select and return strings
 def bitstring(n):
     if n==0:return []
     if n==1:return ["0","1"]
